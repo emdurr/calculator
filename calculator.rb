6f92f5573217d616e261b008ed030794cc70f173
Eric Durr <emdurr@gmail.com>
2016-08-04 10:01:29 -0600

    changes made after class review

--- calculator.rb
+++ calculator.rb
@@ -1,19 +1,17 @@
-x = true
+INVALID_INPUT = 0
+keep_looping = true
 puts "Welcome to the Ruby Calculator!"
 
-def start
-  #@num_1 = num1
-  #opand = operator
-  #num2 = get_two
-  result = @num_1 + @num2
+def run_calc
+  result = @num_1.send(@operand,@num2)
   puts "Calculating......"
   puts "The result of #{@num_1} #{@operand} #{@num2} is #{@num_1.send(@operand, @num2)}"
   puts "Continue with this number? y/n"
-  input = gets.strip
-  input.downcase
+  input = gets.strip.downcase
+
   if input == 'y'
     @num_1 = result
-    puts "\n"
+    puts 
     operator
   else
     num1
@@ -23,43 +21,35 @@ end
 def num1
   puts "\n** Enter exit to quit **\n"
   puts "\nWhat is the first number?"
+
   input1 = gets.strip.downcase
+
   case input1
-  when "exit"
-    exit
-  else
-    @num_1 = input1.to_f
-  end
-  case @num_1
-  when 0
-    puts "*************************************\nInvalid number, please try again.\n*************************************"
-    num1
-    # Restart the method  -  if you re-call the method then it still returns a 0 value
-  else
-    puts "#{@num_1} is the first number."
-    operator
-  end
+    when "exit"
+      exit
+    else
+      @num_1 = input1.to_f
+    end
+
+    case @num_1
+    when INVALID_INPUT
+          puts "*************************************\nInvalid number, please try again.\n*************************************"
+          num1
+    else
+      puts "#{@num_1} is the first number."
+      operator
+    end
 end
 
 def operator
   available_ops = ['+', '-', '*', '/']
   puts "What is the math Operator?"
   op_1 = gets.strip
+
   if available_ops.include? op_1
-    case op_1
-    when '+'
-      @operand = '+'
-      get_two
-    when '-'
-      @operand = '-'
-      get_two
-    when '*'
-      @operand = '*'
-      get_two
-    else
-      @operand = '/'
-      get_two
-    end
+    @operand = op_1
+    get_two
+
   else
     puts "********************************************************\nInvalid Operator. I only allow + - * /. Please try again\n********************************************************"
     operator
@@ -68,17 +58,32 @@ end
 
 def get_two
   puts "What is the next number?"
-  @num2 = gets.strip.to_f
+  @num2 = gets.to_f
   case @num2
-  when 0
+  when INVALID_INPUT
     puts "********************************************************\nInvalid number, please start again.\n********************************************************"
     get_two
   else
     puts "#{@num2} is the next number."
-    start
+    run_calc
   end
 end
 
-while x == true
+while keep_looping == true
   num1
 end
+
+#def string_separator
+#  a = gets.strip
+#  a.match(#pull out everything in first set of ().) == nil ? true : false
+#end
+  # Run code that will separate numbers and operators
+  # RegexOne is set up to do this
+  # Ex: 9 + 4 ( 4 ^ 3  - 15) - 4 / 2
+  # First you would need to run the paranthetical statement ( 4 ^ 3 - 15 )
+  #   So the function would need to pull out that statement first
+    # => Then it would need to do its own PEMDAS
+  # It will first need to separate numbers and put them into an Array -- this won't work because the operators
+    # won't always be in the same place as the numbers
+  # after which it needs to look for PEMDAS in that order
+  # Next it will need to
